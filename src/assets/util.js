0fc3784458df7846a286ebd6c0eadfd6967fa21e
Chaman Rawat <chamanrawat.dev@gmail.com>
2023-12-11 19:50:53 +0530

    added type writer animations

---
+++ src/assets/util.js
@@ -0,0 +1,8 @@
+export const preTrain = `Act as a coding generation model for natural language to python code, I need your expertise with a specific algorithm to covert it to code
+        Please follow the instructions:
+        1. Read each statements of the algorithm
+        2. Generate a working python function
+        3. Sometimes make mistakes intedendly
+        IMPORTANT: Your response should only include the function definition and code solution in plain text format (no backticks, code blocks, or additional formatting).
+        Do not explain your solution or provide any additional information other than the code.
+        Here's the problem description and code:`;
